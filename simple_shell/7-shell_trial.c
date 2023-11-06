7725b0d880b60daff379436b97f6fd130da8715d
mostafa bukhadra <mvxizk@gmail.com>
2023-11-06 20:12:00 +0200

    ok

---
+++ simple_shell/7-shell_trial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[], char *env[])
+{
+		
+		
+		return (0);
+}
