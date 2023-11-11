c75df7b648e42ee19ea5b06807f998d73bf30a9c
mostafa bukhadra <mvxizk@gmail.com>
2023-11-11 02:15:29 +0200

    ok

---
+++ shell_practice/shell_2/simple_shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+#endif
